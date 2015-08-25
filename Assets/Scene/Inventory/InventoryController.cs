dd98d847a2957e63e35e23b972ae09adfbef74e6
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-25 22:21:53 +0900

    make characterboxes in InventoryScene.

--- Assets/Scene/Inventory/InventoryController.cs
+++ Assets/Scene/Inventory/InventoryController.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class InventoryController : MonoBehaviour 
 {
 	public GameObject CharacterSlot;
-	public Transform BoxPosition;
 
 	void Start()
 	{
-		Instantiate (CharacterSlot, BoxPosition.position, BoxPosition.rotation);
 	}
 
 	public void Back()
