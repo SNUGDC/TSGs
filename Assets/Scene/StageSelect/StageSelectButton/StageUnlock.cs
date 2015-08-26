8ab5d6c6f7ce3945a58022396d76873988e567d2
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-26 11:22:56 +0900

    implement stage unlock.

---
+++ Assets/Scene/StageSelect/StageSelectButton/StageUnlock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageUnlock : MonoBehaviour 
+{
+	public int levels;
+
+	void Start () 
+	{
+		for (int i = 0; i < levels; i++) 
+		{
+			if (PlayerPrefs.GetInt("Level" + (i+2))==1)
+			{
+				GameObject.Find("StageLock" + (i+2)).SetActive(false);
+			}
+		}
+	}
+}
