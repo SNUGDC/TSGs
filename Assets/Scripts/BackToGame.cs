4674eed76e9096769286542684c7ac1cb88869bd
Joowhan <darkenir@naver.com>
2015-08-27 02:40:21 +0900

    Game Pause Button Completed

---
+++ Assets/Scripts/BackToGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToGame : MonoBehaviour
+{
+    public GameObject pauseBoard;
+
+    public void ClosePauseBoard()
+    {
+        Time.timeScale = 1;
+        pauseBoard.SetActive(false);
+    }
+
+}
--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class PauseController : MonoBehaviour
 {
+    public GameObject pauseBoard;
     public void Pause()
     {
         Time.timeScale = 0;
+        pauseBoard.SetActive(true);
     }
 }
