8a6a5e58a88df28ac0054e4400f01923445bb9bb
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-29 00:33:48 +0900

    make base for select character.

---
+++ Assets/Scene/Inventory/CharacterBox/DragController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace TSGs.Inventory
+{
+	public class DragController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+	{
+		public static GameObject CharacterBeingDragged;
+		Vector3 StartPosition;
+		Transform StartParent;
+
+		public void OnBeginDrag (PointerEventData EventData)
+		{
+			CharacterBeingDragged = gameObject;
+			StartPosition = transform.position;
+			StartParent = transform.parent;
+			GetComponent<CanvasGroup> ().blocksRaycasts = false;
+		}
+
+		public void OnDrag (PointerEventData EventData)
+		{
+			transform.position = Input.mousePosition;
+		}
+
+		public void OnEndDrag (PointerEventData EventData)
+		{
+			CharacterBeingDragged = null;
+			GetComponent<CanvasGroup> ().blocksRaycasts = true;
+			if(transform.parent == StartParent)
+			transform.position = StartPosition;
+		}
+		
+
+	}
+}
---
+++ Assets/Scene/Inventory/CharacterBox/Slot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace TSGs.Inventory
+{
+	public class Slot : MonoBehaviour, IDropHandler
+	{	
+		public GameObject item
+		{
+			get
+			{
+				if(transform.childCount>0)
+					return transform.GetChild(0).gameObject;
+
+				return null;
+			}
+		}
+
+		public void OnDrop (PointerEventData EventData)
+		{
+			if (!item)
+				DragController.CharacterBeingDragged.transform.SetParent (transform);
+  		}
+	}
+}
