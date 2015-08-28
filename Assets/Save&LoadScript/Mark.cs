c2aba79dcc72ecfd4eb294e1086291f9f102ccbe
wjdghk1867@gmail.com <alswjdghk@snu.ac.kr>
2015-08-28 21:50:07 +0900

    make example script of character.

--- Assets/Save&LoadScript/Mark.cs
+++ Assets/Save&LoadScript/Mark.cs
@@ -1,20 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mark : Character {
-
-	public Mark()
+namespace TSGs
+{
+	public class Mark : Character 
 	{
-		name = "Mark";
-	}
+		public Mark()
+		{
+			Name = "Mark";
+			SetSkill ();
+		}
 
-	public void skill()
-	{
-		GameObject[] bricks = GameObject.FindGameObjectsWithTag ("Brick");
-		int i;
-		for (i=0; i<bricks.Length; i++) {
-			bricks [i].GetComponent<BrickBehavior> ().brickHP--;
-			bricks [i].GetComponent<BrickBehavior> ().DrawBrick ();
+		public void SetSkill()
+		{
+			_skill.Water1 ();
 		}
 	}
 }
